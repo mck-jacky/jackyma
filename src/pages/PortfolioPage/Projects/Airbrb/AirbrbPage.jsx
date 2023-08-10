8c76e20f9e8b79d0ea9d095d4ff9cf3fedfcde8f
mck-jacky <machakki23@gmail.com>
2023-08-10 12:05:38 +1000

    update airbrb

--- src/pages/PortfolioPage/Projects/Airbrb/AirbrbPage.jsx
+++ src/pages/PortfolioPage/Projects/Airbrb/AirbrbPage.jsx
@@ -44,16 +44,16 @@ const AirbrbPage = () => {
             <BsCalendarEvent size={24} className='icon'/>
             UNSW COMP6080 Web Front-End Programming
           </div>
-          <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+          <p>Developed a clone of the popular vaction rental platform, Airbnb, as part of a university assignment.</p>
           <div className='icon-containers'>
             <FaReact size={42} color='#61DAFB' className='icon' title='React'/>
             <SiJavascript size={42} color='#F7DF1E' className='icon' title='Javascript'/> 
           </div>
           <div className='link-containers'>
-            <span>view it on</span>
+            {/* <span>view it on</span>
             <a href="https://github.com/2022hackathon/lighthouse" target="_blank" rel="noopener noreferrer">
               <AiFillGithub size={42} color='#181717' className='icon' title='Github'/> 
-            </a>
+            </a> */}
           </div>
         </div>
       </div>
